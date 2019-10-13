6739f14d05dbc2b090b6527283388b533863a6d4
Tawanda <tmuhwati@gmail.com>
2019-10-13 04:24:00 +0200

    Remove unused resources

--- app/src/main/java/za/co/boxstudio/ui/activities/HomeActivity.java
+++ app/src/main/java/za/co/boxstudio/ui/activities/HomeActivity.java
@@ -1,25 +1,15 @@
 package za.co.boxstudio.ui.activities;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.tabs.TabLayout;
 
-import za.co.boxstudio.ui.fragments.BookingFragment;
-import za.co.boxstudio.ui.fragments.ScheduleFragment;
 import za.co.boxstudio.R;
 
 public class HomeActivity extends AppCompatActivity {
